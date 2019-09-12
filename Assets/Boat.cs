681ebbd10f083c951d13d25e12e546d826b45fcb
Axel Ekwall <axel@minimize.se>
2019-09-12 11:28:57 +0200

    add a boat with some interaction

---
+++ Assets/Boat.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boat : MonoBehaviour
+{
+
+    Rigidbody boat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        boat = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            boat.AddRelativeForce(Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            boat.AddRelativeForce(-Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            boat.AddRelativeForce(Vector3.forward);
+        }
+    }
+}
