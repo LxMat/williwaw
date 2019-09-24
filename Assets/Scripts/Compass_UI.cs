638aaedcf8085da67fea20bc6df5da52151d2ef4
williwaw-agi <chantal_hundscheid@hotmail.com>
2019-09-25 01:58:38 +0200

    Re-designed Environment, Started UI Compass

---
+++ Assets/Scripts/Compass_UI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Compass_UI : MonoBehaviour
+{
+    public Vector3 NorthDirection;
+    public Transform Player;
+
+    public RectTransform Northlayer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeNorthDirection(); 
+    }
+
+    public void ChangeNorthDirection()
+    {
+        NorthDirection.z = Player.eulerAngles.y;
+        Northlayer.localEulerAngles = NorthDirection;
+    }
+
+}
