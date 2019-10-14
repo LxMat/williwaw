e008fc9cac4f9713f94c2bf77d16e5a0b27cd0c7
Alexander <fight4real@hotmail.com>
2019-10-14 14:11:17 +0200

    [WIP] Loads new boat to scene

--- Assets/Boat/Boat.cs
+++ Assets/Boat/Boat.cs
@@ -18,18 +18,20 @@ public class Boat : NetworkBehaviour
     private bool development = false;
     private WaterPlane waves;
 
+
     // Start is called before the first frame update
     private void Start()
     {
         boat = GetComponent<Rigidbody>();
         direction = new Vector3();
         development = false;
+        transform.rotation = Quaternion.identity; //for some reason the boat spawns sideways...
 
     }
 
     public override void OnStartLocalPlayer()
     {
-        Camera.main.GetComponent<SmoothFollow>().target = follow.transform;
+        //Camera.main.GetComponent<SmoothFollow>().target = follow.transform;
     }
 
     //Awake is called after all objects are initialized.
@@ -38,6 +40,20 @@ public class Boat : NetworkBehaviour
         waves = GameObject.Find("Waves").GetComponent<WaterPlane>();
         micObject = GameObject.Find("Microphone");
         gyroObject = GameObject.Find("Gyroscope");
+    }
+
+    Vector3 getNormal(Vector3 p0, Vector3 p1, Vector3 p2)
+    {
+        Vector3 u = p1 - p0;
+        Vector3 v = p2 - p1;
+        return Vector3.Cross(u, v).normalized;
+    }
+
+    private void setHeight(Transform gameObj)
+    {
+
+        Vector3 current = gameObj.position;
+        gameObj.position = new Vector3(current.x, waves.getHeight(gameObj.position), current.z);
     }
 
 
@@ -49,15 +65,16 @@ public class Boat : NetworkBehaviour
 
             Vector3 currentRot = transform.rotation.eulerAngles;
             Vector3 currentPos = transform.position;
-            Vector3 newPos = new Vector3(currentPos.x, waves.getHeight(currentPos), currentPos.z);
-            transform.position = newPos;
-            transform.rotation = Quaternion.Euler(new Vector3(-90, currentRot.y, currentRot.z)); // stops boat from flipping around.
+            //Vector3 newPos = new Vector3(currentPos.x, waves.getHeight(currentPos), currentPos.z);
+            //transform.position = newPos;
+            //            transform.rotation = Quaternion.Euler(new Vector3(-90, currentRot.y, currentRot.z)); // stops boat from flipping around.
 
 
             force = micObject.GetComponent<MicrophoneInput>().force;
             rotation = -gyroObject.GetComponent<GyroscopeInput>().rotation;
             direction.z = -rotation;//Mathf.Sin(rotation);
                                     // direction.z = Mathf.Cos(rotation);
+
             forceVector = Vector3.up * force * 2.0f;
 
             if (boat.velocity.magnitude < 50)
@@ -69,44 +86,45 @@ public class Boat : NetworkBehaviour
             //boat.AddTorque(direction);
 
             //transform.Rotate(direction, Space.Self);
-
             if (Input.GetKey(KeyCode.O))
             {
                 development = !development;
             }
-            if (development)
+            if (true)
             {
                 if (Input.GetKey(KeyCode.W))
                 {
-                    if (boat.velocity.magnitude < 5)
-                    {
-                        boat.AddRelativeForce(Vector3.up * 2);
-                    }
+                        boat.AddRelativeForce(-Vector3.right);
+                    
                 }
                 if (Input.GetKey(KeyCode.A))
                 {
-                    transform.Rotate(-Vector3.forward);
+                    transform.Rotate(-Vector3.up);
                 }
-                if (Input.GetKey(KeyCode.D))
+               if (Input.GetKey(KeyCode.D))
                 {
-                    transform.Rotate(Vector3.forward);
+                    transform.Rotate(Vector3.up);
                 }
-                if (Input.GetKey(KeyCode.S))
+               if (Input.GetKey(KeyCode.S))
                 {
-                    boat.AddRelativeForce(-Vector3.up);
+                    boat.AddRelativeForce(Vector3.right);
                 }
+               
+               
+                    boat.velocity = boat.velocity * 0.995f;
+               
             }
-            else
-            {
-                if (rotation < -0.2f)
-                {
-                    transform.Rotate(Vector3.forward);
-                }
-                if (rotation > 0.2f)
-                {
-                    transform.Rotate(-Vector3.forward);
-                }
-            }
+            //else
+            //{
+            //    if (rotation < -0.2f)
+            //    {
+            //        transform.Rotate(Vector3.forward);
+            //    }
+            //    if (rotation > 0.2f)
+            //    {
+            //        transform.Rotate(-Vector3.forward);
+            //    }
+            //}
         }
     }
 }
