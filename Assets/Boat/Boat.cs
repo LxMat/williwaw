a8077b1e2ac00188e0480f8652080fd85e7b764e
Alexander <fight4real@hotmail.com>
2019-10-14 15:20:29 +0200

    [WIP] working on camera distance to boat speed

--- Assets/Boat/Boat.cs
+++ Assets/Boat/Boat.cs
@@ -17,8 +17,10 @@ public class Boat : NetworkBehaviour
     private Vector3 direction;
     private bool development = false;
     private WaterPlane waves;
-
-
+    private float cameraDistanceInit;
+    private float cameraDistanceSpeedUp = 1000f;
+    private SmoothFollow boatCamera;
+    private float currentCameraDistance;
     // Start is called before the first frame update
     private void Start()
     {
@@ -26,12 +28,16 @@ public class Boat : NetworkBehaviour
         direction = new Vector3();
         development = false;
         transform.rotation = Quaternion.identity; //for some reason the boat spawns sideways...
-
+        boatCamera = Camera.main.GetComponent<SmoothFollow>();
+        cameraDistanceInit = boatCamera.distance;
     }
 
     public override void OnStartLocalPlayer()
     {
-        //Camera.main.GetComponent<SmoothFollow>().target = follow.transform;
+        Camera.main.GetComponent<SmoothFollow>().target = follow.transform;
+        cameraDistanceInit = currentCameraDistance = Camera.main.GetComponent<SmoothFollow>().distance;
+        
+        
     }
 
     //Awake is called after all objects are initialized.
@@ -69,7 +75,7 @@ public class Boat : NetworkBehaviour
             //transform.position = newPos;
             //            transform.rotation = Quaternion.Euler(new Vector3(-90, currentRot.y, currentRot.z)); // stops boat from flipping around.
 
-
+            setHeight(transform);
             force = micObject.GetComponent<MicrophoneInput>().force;
             rotation = -gyroObject.GetComponent<GyroscopeInput>().rotation;
             direction.z = -rotation;//Mathf.Sin(rotation);
@@ -79,14 +85,33 @@ public class Boat : NetworkBehaviour
 
             if (boat.velocity.magnitude < 50)
             {
+                
                 boat.AddRelativeForce(forceVector);
             }
 
 
-            //boat.AddTorque(direction);
 
-            //transform.Rotate(direction, Space.Self);
-            if (Input.GetKey(KeyCode.O))
+            //if the boat speeds up the camera moves further away and as is slows down the camera gets closer.
+
+            Debug.Log(boat.velocity.magnitude);
+            if (boat.velocity.magnitude > 50 && currentCameraDistance < (cameraDistanceSpeedUp)) 
+            {
+                
+                //boatCamera.distance = Mathf.Lerp(cameraDistanceInit, cameraDistanceSpeedUp, Time.deltaTime / 10);
+                boatCamera.distance = Mathf.Lerp(boatCamera.distance,200f,Time.deltaTime/10);
+                currentCameraDistance = Camera.main.GetComponent<SmoothFollow>().distance;
+            }else if (boat.velocity.magnitude <= 50 && currentCameraDistance != cameraDistanceInit)
+            {
+                //boatCamera.distance = currentCameraDistance = cameraDistanceInit;
+                boatCamera.distance = Mathf.Lerp(boatCamera.distance, cameraDistanceInit, Time.deltaTime);
+                currentCameraDistance = boatCamera.distance;
+            }
+
+                    //boat.AddTorque(direction);
+
+                    //transform.Rotate(direction, Space.Self);
+
+                    if (Input.GetKey(KeyCode.O))
             {
                 development = !development;
             }
