7ac28b3d7f0ae41dd0fa8748b0cf764a7a9a831d
Axel Ekwall <axel@minimize.se>
2019-10-22 12:51:30 +0200

    Add map marker dummy

---
+++ Assets/Boat/MapMarker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+using UnityEngine;
+
+[System.Obsolete]
+public class MapMarker : NetworkBehaviour
+{
+
+    public GameObject Marker;
+
+    // Update is called once per frame
+
+    public override void OnStartLocalPlayer()
+    {
+        Marker.SetActive(false);
+    }
+
+    private void Start()
+    {
+        Material material = new Material(Shader.Find("Standard"));
+        Color color = new Color(200, 50, 50, 1);
+        material.color = color;
+        Marker.GetComponent<MeshRenderer>().material = material;
+    }
+}
