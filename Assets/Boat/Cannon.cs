96d2a15fdd89a46055ab763ec2c1c95056b76d81
Axel Ekwall <axel@minimize.se>
2019-10-22 11:53:24 +0200

    better cannon fire parameters

--- Assets/Boat/Cannon.cs
+++ Assets/Boat/Cannon.cs
@@ -9,9 +9,21 @@ public class Cannon : NetworkBehaviour
     public float firePower;
     public float fireAngle;
     public GameObject projectile;
+    private float up;
+    private float forward;
+    private float fireAngleRad;
 
     // Update is called once per frame
-    void Update()
+    private void Start()
+    {
+        fireAngleRad = (fireAngle * Mathf.PI) / 180;
+        up = Mathf.Sin(fireAngleRad) * firePower;
+        forward = Mathf.Cos(fireAngleRad) * firePower;
+        Debug.Log("up" + up);
+        Debug.Log("forward" + forward);
+    }
+
+    private void Update()
     {
         if (Input.GetKey(KeyCode.Space) && nextAttack < Time.time)
         {
@@ -29,12 +41,8 @@ public class Cannon : NetworkBehaviour
     [Command]
     void CmdFire()
     {
-        float up = Mathf.Cos(fireAngle) * firePower;
-        float forward = Mathf.Sin(fireAngle) * firePower;
-        Debug.Log("up" + up);
-        Debug.Log("forward" + forward);
         GameObject cannonBall = Instantiate(projectile, transform.position + transform.right * -10, transform.rotation);
-        cannonBall.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(- firePower, firePower/10, 0));
+        cannonBall.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(-forward, up, 0));
         NetworkServer.Spawn(cannonBall);
         Debug.Log("BOOM");
     }
