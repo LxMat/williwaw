7f24f8e7673b3fb915838754ec5fb817c0bbe81a
Alexander <fight4real@hotmail.com>
2019-10-30 14:01:24 +0100

    Added WIndCompass

---
+++ Assets/Boat/UI/WindAnimation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class WindAnimation : MonoBehaviour
+{
+    private Image windIcon;
+    private bool imageFilled;
+    public float animationSpeed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        windIcon = gameObject.GetComponent<Image>();
+        windIcon.fillAmount = 1;
+        imageFilled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (imageFilled)
+        {
+            windIcon.fillAmount = 0;
+            imageFilled = false;
+        }else
+        {
+            windIcon.fillAmount += 1 / animationSpeed * Time.deltaTime;
+        }
+        if( windIcon.fillAmount >= 1)
+        {
+            imageFilled = true;
+        }
+        
+    }
+}
